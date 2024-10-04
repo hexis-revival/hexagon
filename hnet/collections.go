ecb6f3b459415719c023bcb4a083d66126a43a13
Lekuru <contact@lekuru.xyz>
2024-10-04 15:30:51 +0200

    Add player collection class

---
+++ hnet/collections.go
@@ -0,0 +1,53 @@
+package hnet
+
+type PlayerCollection struct {
+	idMap   map[uint32]*Player
+	nameMap map[string]*Player
+}
+
+func (collection *PlayerCollection) Add(player *Player) {
+	collection.idMap[player.Id] = player
+	collection.nameMap[player.Name] = player
+}
+
+func (collection *PlayerCollection) Remove(player *Player) {
+	delete(collection.idMap, player.Id)
+	delete(collection.nameMap, player.Name)
+}
+
+func (collection *PlayerCollection) Count() int {
+	return len(collection.idMap)
+}
+
+func (collection *PlayerCollection) ByID(id uint32) *Player {
+	if val, ok := collection.idMap[id]; ok {
+		return val
+	}
+
+	return nil
+}
+
+func (collection *PlayerCollection) ByName(name string) *Player {
+	if val, ok := collection.nameMap[name]; ok {
+		return val
+	}
+
+	return nil
+}
+
+func (collection *PlayerCollection) All() []Player {
+	players := make([]Player, 0, len(collection.idMap))
+
+	for _, player := range collection.idMap {
+		players = append(players, *player)
+	}
+
+	return players
+}
+
+func NewPlayerCollection() PlayerCollection {
+	return PlayerCollection{
+		idMap:   make(map[uint32]*Player),
+		nameMap: make(map[string]*Player),
+	}
+}
