f056f422cfe20e9d72e1fa639afce38e1d7669b4
Lekuru <contact@lekuru.xyz>
2024-10-04 16:12:30 +0200

    Add logging middleware to hscore

--- hscore/server.go
+++ hscore/server.go
@@ -3,6 +3,7 @@ package hscore
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/lekuruu/hexagon/common"
 )
@@ -11,6 +12,7 @@ type ScoreServer struct {
 	Host   string
 	Port   int
 	Logger *common.Logger
+	mux    *http.ServeMux
 }
 
 type Context struct {
@@ -23,8 +25,11 @@ func (server *ScoreServer) Serve() {
 	bind := fmt.Sprintf("%s:%d", server.Host, server.Port)
 	server.Logger.Infof("Listening on %s", bind)
 
-	http.HandleFunc("/score/submit", withContext(ScoreSubmissionHandler))
-	http.ListenAndServe(bind, nil)
+	server.mux = http.NewServeMux()
+	server.mux.HandleFunc("/score/submit", server.contextMiddleware(ScoreSubmissionHandler))
+
+	loggedMux := server.loggingMiddleware(server.mux)
+	http.ListenAndServe(bind, loggedMux)
 }
 
 func NewServer(host string, port int, logger *common.Logger) *ScoreServer {
@@ -35,14 +40,30 @@ func NewServer(host string, port int, logger *common.Logger) *ScoreServer {
 	}
 }
 
-func withContext(handler func(*Context)) http.HandlerFunc {
+func (server *ScoreServer) contextMiddleware(handler func(*Context)) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		context := &Context{
 			Response: w,
 			Request:  r,
-			Server:   nil,
+			Server:   server,
 		}
 
 		handler(context)
 	}
+}
+
+func (server *ScoreServer) loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+		next.ServeHTTP(w, r)
+		time := time.Since(start)
+
+		server.Logger.Infof(
+			"%s - %s %s (%v)",
+			r.RemoteAddr,
+			r.Method,
+			r.URL.Path,
+			time,
+		)
+	})
 }
