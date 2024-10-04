17f019090fccdc78e510ad37b3519c77193faaa5
Lekuru <contact@lekuru.xyz>
2024-10-04 15:53:29 +0200

    Add hscore server to `main.go`

--- go.mod
+++ go.mod
@@ -3,15 +3,18 @@ module github.com/lekuruu/hexagon
 go 1.22.7
 
 require (
-	github.com/lekuruu/hexagon/common v0.0.0-20241003124433-ad9d5dc7b056
+	github.com/lekuruu/hexagon/common v0.0.0-20241004134151-9a352d6c7cd7
 	github.com/lekuruu/hexagon/hnet v0.0.0-00010101000000-000000000000
 )
 
 require (
 	github.com/lekuruu/go-raknet v0.0.0-20241003121121-43b332df40f2 // indirect
+	github.com/lekuruu/hexagon/hscore v0.0.0-20241004135010-1d8d562aeb0e
 	golang.org/x/exp v0.0.0-20240909161429-701f63a606c0 // indirect
 )
 
 replace github.com/lekuruu/hexagon/hnet => ./hnet
 
 replace github.com/lekuruu/hexagon/common => ./common
+
+replace github.com/lekuruu/hexagon/hscore => ./hscore
--- main.go
+++ main.go
@@ -1,17 +1,40 @@
 package main
 
 import (
+	"sync"
+
 	"github.com/lekuruu/hexagon/common"
 	"github.com/lekuruu/hexagon/hnet"
+	"github.com/lekuruu/hexagon/hscore"
 )
+
+func runService(wg *sync.WaitGroup, worker func()) {
+	wg.Add(1)
+
+	go func() {
+		defer wg.Done()
+		worker()
+	}()
+}
 
 func main() {
 	// TODO: Add command line arguments for host and port
-	server := hnet.NewServer(
+	hnetServer := hnet.NewServer(
 		"0.0.0.0",
 		21556,
 		common.CreateLogger("hnet", common.DEBUG),
 	)
 
-	server.Serve()
+	hscoreServer := hscore.NewServer(
+		"0.0.0.0",
+		80,
+		common.CreateLogger("hscore", common.DEBUG),
+	)
+
+	var wg sync.WaitGroup
+
+	runService(&wg, hnetServer.Serve)
+	runService(&wg, hscoreServer.Serve)
+
+	wg.Wait()
 }
