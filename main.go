bc009280b534c831f5d3f8f4c83f2a805687b225
Lekuru <contact@lekuru.xyz>
2024-10-05 00:07:59 +0200

    Add config using command line arguments

--- main.go
+++ main.go
@@ -1,12 +1,38 @@
 package main
 
 import (
+	"flag"
 	"sync"
 
 	"github.com/lekuruu/hexagon/common"
 	"github.com/lekuruu/hexagon/hnet"
 	"github.com/lekuruu/hexagon/hscore"
 )
+
+type Config struct {
+	HNet struct {
+		Host string
+		Port int
+	}
+	HScore struct {
+		Host string
+		Port int
+	}
+}
+
+func loadConfig() Config {
+	var config Config
+
+	flag.StringVar(&config.HNet.Host, "hnet-host", "0.0.0.0", "Host for the hnet server")
+	flag.IntVar(&config.HNet.Port, "hnet-port", 21556, "Port for the hnet server")
+
+	flag.StringVar(&config.HScore.Host, "hscore-host", "0.0.0.0", "Host for the hscore server")
+	flag.IntVar(&config.HScore.Port, "hscore-port", 80, "Port for the hscore server")
+
+	flag.Parse()
+
+	return config
+}
 
 func runService(wg *sync.WaitGroup, worker func()) {
 	wg.Add(1)
@@ -18,16 +44,17 @@ func runService(wg *sync.WaitGroup, worker func()) {
 }
 
 func main() {
-	// TODO: Add command line arguments for host and port
+	config := loadConfig()
+
 	hnetServer := hnet.NewServer(
-		"0.0.0.0",
-		21556,
+		config.HNet.Host,
+		config.HNet.Port,
 		common.CreateLogger("hnet", common.DEBUG),
 	)
 
 	hscoreServer := hscore.NewServer(
-		"0.0.0.0",
-		80,
+		config.HScore.Host,
+		config.HScore.Port,
 		common.CreateLogger("hscore", common.DEBUG),
 	)
 
