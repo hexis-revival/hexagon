b3e3d45b41250d686d3677583d4b6fcace964860
tree <tree@ascension.wtf>
2024-10-07 13:45:57 +0000

    Add all `action` types, and friends list packet ID

--- hnet/constants.go
+++ hnet/constants.go
@@ -10,11 +10,16 @@ const (
 	SERVER_LOGIN_RESPONSE uint32 = 2
 	SERVER_USER_STATS     uint32 = 5
 	SERVER_USER_INFO      uint32 = 6
+	SERVER_FRIENDS_LIST   uint32 = 8
 )
 
 const (
-	ACTION_IDLE    uint32 = 1
-	ACTION_PLAYING uint32 = 3
-	ACTION_EDITING uint32 = 5
-	ACTION_TESTING uint32 = 6
+	ACTION_IDLE       uint32 = 1
+	ACTION_AWAY       uint32 = 2
+	ACTION_PLAYING    uint32 = 3
+	ACTION_EDITING    uint32 = 4
+	ACTION_MODDING    uint32 = 5
+	ACTION_TESTING    uint32 = 6
+	ACTION_SUBMITTING uint32 = 7
+	ACTION_WATCHING   uint32 = 8
 )
