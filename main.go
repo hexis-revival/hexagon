b6e2fdb5a22c5c9da604a8c124dc903c3e59882d
Lekuru <contact@lekuru.xyz>
2024-10-08 01:23:59 +0200

    Add state configuration arguments

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"sync"
 
 	"github.com/lekuruu/hexagon/common"
@@ -18,10 +19,12 @@ type Config struct {
 		Host string
 		Port int
 	}
+	State *common.StateConfiguration
 }
 
 func loadConfig() Config {
 	var config Config
+	config.State = common.NewStateConfiguration()
 
 	flag.StringVar(&config.HNet.Host, "hnet-host", "0.0.0.0", "Host for the hnet server")
 	flag.IntVar(&config.HNet.Port, "hnet-port", 21556, "Port for the hnet server")
@@ -29,6 +32,17 @@ func loadConfig() Config {
 	flag.StringVar(&config.HScore.Host, "hscore-host", "0.0.0.0", "Host for the hscore server")
 	flag.IntVar(&config.HScore.Port, "hscore-port", 80, "Port for the hscore server")
 
+	flag.StringVar(&config.State.Database.Host, "db-host", "localhost", "Database host")
+	flag.IntVar(&config.State.Database.Port, "db-port", 5432, "Database port")
+	flag.StringVar(&config.State.Database.Username, "db-username", "postgres", "Database username")
+	flag.StringVar(&config.State.Database.Password, "db-password", "postgres", "Database password")
+	flag.StringVar(&config.State.Database.Database, "db-database", "hexagon", "Database name")
+
+	flag.IntVar(&config.State.Database.MaxIdle, "db-max-idle", 10, "Database max idle connections")
+	flag.IntVar(&config.State.Database.MaxOpen, "db-max-open", 100, "Database max open connections")
+	flag.DurationVar(&config.State.Database.MaxLifetime, "db-max-lifetime", 0, "Database max connection lifetime")
+
+	flag.StringVar(&config.State.DataPath, "data-path", ".data", "Path to store data")
 	flag.Parse()
 
 	return config
