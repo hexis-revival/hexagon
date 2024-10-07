c1f8b250e18c0d838f9cec683babfa8deb1639c1
Lekuru <contact@lekuru.xyz>
2024-10-08 01:21:54 +0200

    Add `State` struct for state management

---
+++ common/state.go
@@ -0,0 +1,41 @@
+package common
+
+import (
+	"gorm.io/gorm"
+)
+
+type StateConfiguration struct {
+	Database *DatabaseConfiguration
+	DataPath string
+}
+
+func NewStateConfiguration() *StateConfiguration {
+	return &StateConfiguration{
+		Database: &DatabaseConfiguration{},
+		DataPath: ".data",
+	}
+}
+
+type State struct {
+	Database *gorm.DB
+	Storage  Storage
+}
+
+func NewState(config *StateConfiguration) (*State, error) {
+	db, err := CreateDatabaseSession(config.Database)
+	if err != nil {
+		return nil, err
+	}
+
+	err = DoDatabaseMigrations(db)
+	if err != nil {
+		return nil, err
+	}
+
+	storage := NewFileStorage(config.DataPath)
+
+	return &State{
+		Database: db,
+		Storage:  storage,
+	}, nil
+}
