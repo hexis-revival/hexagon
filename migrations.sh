8899acf339a56e6432f51a2c4ef4eff44a9964f5
Lekuru <contact@lekuru.xyz>
2024-10-08 02:18:27 +0200

    Add check for `migrate` command

--- migrations.sh
+++ migrations.sh
@@ -9,4 +9,10 @@ fi
 DATABASE_URL="postgres://${POSTGRES_USER}:${POSTGRES_PASSWORD}@127.0.0.1:${POSTGRES_PORT}/${POSTGRES_DB}?sslmode=disable"
 MIGRATIONS_DIR="./migrations"
 
+if ! [ -x "$(command -v migrate)" ]; then
+  echo 'Error: go-migrate is not installed.' >&2
+  echo 'Please follow their installation guide: https://github.com/golang-migrate/migrate/blob/master/cmd/migrate/README.md#installation' >&2
+  exit 1
+fi
+
 migrate -database "$DATABASE_URL" -path "$MIGRATIONS_DIR" $@
