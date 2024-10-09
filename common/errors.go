e122bf3c0d807209a08a09e7a75e56eb7ee77af6
Lekuru <contact@lekuru.xyz>
2024-10-09 13:40:05 +0200

    Add error collection struct

---
+++ common/errors.go
@@ -0,0 +1,31 @@
+package common
+
+type ErrorCollection struct {
+	errors []error
+}
+
+func (ec *ErrorCollection) Add(err error) {
+	if err == nil {
+		return
+	}
+	ec.errors = append(ec.errors, err)
+}
+
+func (ec *ErrorCollection) FirstError() error {
+	if !ec.HasErrors() {
+		return nil
+	}
+	return ec.errors[0]
+}
+
+func (ec *ErrorCollection) Errors() []error {
+	return ec.errors
+}
+
+func (ec *ErrorCollection) HasErrors() bool {
+	return len(ec.errors) > 0
+}
+
+func NewErrorCollection() *ErrorCollection {
+	return &ErrorCollection{}
+}
