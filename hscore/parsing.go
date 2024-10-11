ddd73edf111f8bc0bee4801fd756d007d6c2cce5
Lekuru <contact@lekuru.xyz>
2024-10-11 19:10:59 +0200

    Move parsing functions to seperate `parsing.go` file

---
+++ hscore/parsing.go
@@ -0,0 +1,138 @@
+package hscore
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/hexis-revival/hexagon/common"
+)
+
+func ParseScoreData(scoreDataBytes []byte) (*ScoreData, error) {
+	scoreData := strings.Split(string(scoreDataBytes), ";")
+
+	if len(scoreData) != 20 {
+		return nil, fmt.Errorf("invalid score data: %d fields", len(scoreData))
+	}
+
+	beatmapChecksum := scoreData[0]
+	scoreChecksum := scoreData[3]
+	username := scoreData[1]
+
+	passed := scoreData[4] == "1"
+	perfect := scoreData[5] == "1"
+
+	unknown1 := scoreData[2]
+	unknown2 := scoreData[19]
+
+	collection := common.NewErrorCollection()
+
+	time, err := strconv.Atoi(scoreData[6])
+	collection.Add(err)
+
+	maxCombo, err := strconv.Atoi(scoreData[7])
+	collection.Add(err)
+
+	totalScore, err := strconv.Atoi(scoreData[8])
+	collection.Add(err)
+
+	count300, err := strconv.Atoi(scoreData[9])
+	collection.Add(err)
+
+	count100, err := strconv.Atoi(scoreData[10])
+	collection.Add(err)
+
+	count50, err := strconv.Atoi(scoreData[11])
+	collection.Add(err)
+
+	countGeki, err := strconv.Atoi(scoreData[12])
+	collection.Add(err)
+
+	countKatu, err := strconv.Atoi(scoreData[13])
+	collection.Add(err)
+
+	countGood, err := strconv.Atoi(scoreData[14])
+	collection.Add(err)
+
+	countMiss, err := strconv.Atoi(scoreData[15])
+	collection.Add(err)
+
+	clientVersion, err := strconv.Atoi(scoreData[18])
+	collection.Add(err)
+
+	mods, err := ParseModsData(scoreData[17])
+	collection.Add(err)
+
+	if collection.HasErrors() {
+		return nil, collection.Pop(0)
+	}
+
+	return &ScoreData{
+		BeatmapChecksum: beatmapChecksum,
+		ScoreChecksum:   scoreChecksum,
+		Username:        username,
+		Passed:          passed,
+		Perfect:         perfect,
+		Time:            time,
+		MaxCombo:        maxCombo,
+		TotalScore:      totalScore,
+		Count300:        count300,
+		Count100:        count100,
+		Count50:         count50,
+		CountGeki:       countGeki,
+		CountKatu:       countKatu,
+		CountGood:       countGood,
+		CountMiss:       countMiss,
+		ClientVersion:   clientVersion,
+		Unknown1:        unknown1,
+		Unknown2:        unknown2,
+		Mods:            mods,
+	}, nil
+}
+
+func ParseModsData(modsString string) (*Mods, error) {
+	modData := strings.Split(modsString, ":")
+	collection := common.NewErrorCollection()
+
+	arOffset, err := strconv.Atoi(modData[0])
+	collection.Add(err)
+
+	odOffset, err := strconv.Atoi(modData[1])
+	collection.Add(err)
+
+	csOffset, err := strconv.Atoi(modData[2])
+	collection.Add(err)
+
+	hpOffset, err := strconv.Atoi(modData[3])
+	collection.Add(err)
+
+	playSpeedMultiplier, err := strconv.Atoi(modData[4])
+	collection.Add(err)
+
+	if collection.HasErrors() {
+		return nil, collection.Pop(0)
+	}
+
+	playSpeed := 1 + (0.5 * float32(playSpeedMultiplier) / 10)
+	hidden := modData[5] == "1"
+	noFail := modData[6] == "1"
+	auto := modData[7] == "1"
+
+	return &Mods{
+		ArOffset:  arOffset,
+		OdOffset:  odOffset,
+		CsOffset:  csOffset,
+		HpOffset:  hpOffset,
+		PlaySpeed: playSpeed,
+		Hidden:    hidden,
+		NoFail:    noFail,
+		Auto:      auto,
+	}, nil
+}
+
+func ParseProcessList(processListBytes []byte) []string {
+	processListStr := strings.ReplaceAll(string(processListBytes), "\n", "")
+	processListStr = strings.ReplaceAll(processListStr, "; ", "")
+	processList := strings.Split(processListStr, "| ")
+	return processList
+}
--- hscore/scoring.go
+++ hscore/scoring.go
@@ -4,13 +4,31 @@ import (
 	"encoding/base64"
 	"encoding/binary"
 	"encoding/json"
-	"fmt"
 	"net/http"
-	"strconv"
-	"strings"
 
 	"github.com/hexis-revival/hexagon/common"
 )
+
+func ScoreSubmissionHandler(ctx *Context) {
+	// Parse score submission request
+	req, err := NewScoreSubmissionRequest(ctx.Request)
+
+	if err != nil {
+		ctx.Server.Logger.Errorf("Error parsing score submission request: %v", err)
+		ctx.Response.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	ctx.Server.Logger.Debugf("Score submission request: %s", req.String())
+	// TODO: Process data & update player stats
+
+	ctx.Response.WriteHeader(http.StatusOK)
+	ctx.Response.Header().Set("Content-Type", "application/json")
+
+	// Write response
+	resp := ScoreSubmissionResponse{Success: true}
+	json.NewEncoder(ctx.Response).Encode(resp)
+}
 
 func NewScoreSubmissionRequest(request *http.Request) (*ScoreSubmissionRequest, error) {
 	err := request.ParseMultipartForm(10 << 20) // ~10 MB
@@ -76,154 +94,4 @@ func NewScoreSubmissionRequest(request *http.Request) (*ScoreSubmissionRequest, 
 		ScoreData:   scoreDataStruct,
 		ClientData:  string(clientDataDecrypted),
 	}, nil
-}
-
-func ParseScoreData(scoreDataBytes []byte) (*ScoreData, error) {
-	scoreData := strings.Split(string(scoreDataBytes), ";")
-
-	if len(scoreData) != 20 {
-		return nil, fmt.Errorf("invalid score data: %d fields", len(scoreData))
-	}
-
-	beatmapChecksum := scoreData[0]
-	scoreChecksum := scoreData[3]
-	username := scoreData[1]
-
-	passed := scoreData[4] == "1"
-	perfect := scoreData[5] == "1"
-
-	unknown1 := scoreData[2]
-	unknown2 := scoreData[19]
-
-	collection := common.NewErrorCollection()
-
-	time, err := strconv.Atoi(scoreData[6])
-	collection.Add(err)
-
-	maxCombo, err := strconv.Atoi(scoreData[7])
-	collection.Add(err)
-
-	totalScore, err := strconv.Atoi(scoreData[8])
-	collection.Add(err)
-
-	count300, err := strconv.Atoi(scoreData[9])
-	collection.Add(err)
-
-	count100, err := strconv.Atoi(scoreData[10])
-	collection.Add(err)
-
-	count50, err := strconv.Atoi(scoreData[11])
-	collection.Add(err)
-
-	countGeki, err := strconv.Atoi(scoreData[12])
-	collection.Add(err)
-
-	countKatu, err := strconv.Atoi(scoreData[13])
-	collection.Add(err)
-
-	countGood, err := strconv.Atoi(scoreData[14])
-	collection.Add(err)
-
-	countMiss, err := strconv.Atoi(scoreData[15])
-	collection.Add(err)
-
-	clientVersion, err := strconv.Atoi(scoreData[18])
-	collection.Add(err)
-
-	mods, err := ParseModsData(scoreData[17])
-	collection.Add(err)
-
-	if collection.HasErrors() {
-		return nil, collection.Pop(0)
-	}
-
-	return &ScoreData{
-		BeatmapChecksum: beatmapChecksum,
-		ScoreChecksum:   scoreChecksum,
-		Username:        username,
-		Passed:          passed,
-		Perfect:         perfect,
-		Time:            time,
-		MaxCombo:        maxCombo,
-		TotalScore:      totalScore,
-		Count300:        count300,
-		Count100:        count100,
-		Count50:         count50,
-		CountGeki:       countGeki,
-		CountKatu:       countKatu,
-		CountGood:       countGood,
-		CountMiss:       countMiss,
-		ClientVersion:   clientVersion,
-		Unknown1:        unknown1,
-		Unknown2:        unknown2,
-		Mods:            mods,
-	}, nil
-}
-
-func ParseModsData(modsString string) (*Mods, error) {
-	modData := strings.Split(modsString, ":")
-	collection := common.NewErrorCollection()
-
-	arOffset, err := strconv.Atoi(modData[0])
-	collection.Add(err)
-
-	odOffset, err := strconv.Atoi(modData[1])
-	collection.Add(err)
-
-	csOffset, err := strconv.Atoi(modData[2])
-	collection.Add(err)
-
-	hpOffset, err := strconv.Atoi(modData[3])
-	collection.Add(err)
-
-	playSpeedMultiplier, err := strconv.Atoi(modData[4])
-	collection.Add(err)
-
-	if collection.HasErrors() {
-		return nil, collection.Pop(0)
-	}
-
-	playSpeed := 1 + (0.5 * float32(playSpeedMultiplier) / 10)
-	hidden := modData[5] == "1"
-	noFail := modData[6] == "1"
-	auto := modData[7] == "1"
-
-	return &Mods{
-		ArOffset:  arOffset,
-		OdOffset:  odOffset,
-		CsOffset:  csOffset,
-		HpOffset:  hpOffset,
-		PlaySpeed: playSpeed,
-		Hidden:    hidden,
-		NoFail:    noFail,
-		Auto:      auto,
-	}, nil
-}
-
-func ParseProcessList(processListBytes []byte) []string {
-	processListStr := strings.ReplaceAll(string(processListBytes), "\n", "")
-	processListStr = strings.ReplaceAll(processListStr, "; ", "")
-	processList := strings.Split(processListStr, "| ")
-	return processList
-}
-
-func ScoreSubmissionHandler(ctx *Context) {
-	// Parse score submission request
-	req, err := NewScoreSubmissionRequest(ctx.Request)
-
-	if err != nil {
-		ctx.Server.Logger.Errorf("Error parsing score submission request: %v", err)
-		ctx.Response.WriteHeader(http.StatusBadRequest)
-		return
-	}
-
-	ctx.Server.Logger.Debugf("Score submission request: %s", req.String())
-	// TODO: Process data & update player stats
-
-	ctx.Response.WriteHeader(http.StatusOK)
-	ctx.Response.Header().Set("Content-Type", "application/json")
-
-	// Write response
-	resp := ScoreSubmissionResponse{Success: true}
-	json.NewEncoder(ctx.Response).Encode(resp)
 }
