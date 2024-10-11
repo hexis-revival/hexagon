d0c3e4d2d4efb17ba181918de3c1ba6e959fe6e6
Lekuru <contact@lekuru.xyz>
2024-10-11 15:39:46 +0200

    Add password/bcrypt utils

---
+++ common/passwords.go
@@ -0,0 +1,70 @@
+package common
+
+import (
+	"crypto/sha512"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+var passwordCache = map[string]bool{}
+
+func GetPasswordCache() map[string]bool {
+	return passwordCache
+}
+
+func ClearPasswordCache() {
+	passwordCache = map[string]bool{}
+}
+
+func CreatePasswordHash(password string) (string, error) {
+	hashedPassword := GetSHA512Hash(password)
+	hashedBytes, err := bcrypt.GenerateFromPassword(
+		hashedPassword,
+		bcrypt.DefaultCost,
+	)
+
+	return string(hashedBytes), err
+}
+
+func CheckPassword(input string, bcryptString string) bool {
+	inputHashed := GetSHA512Hash(input)
+
+	if isCorrect, ok := passwordCache[string(inputHashed)]; ok {
+		return isCorrect
+	}
+
+	err := bcrypt.CompareHashAndPassword(
+		[]byte(bcryptString),
+		inputHashed,
+	)
+
+	isCorrect := err == nil
+	passwordCache[string(inputHashed)] = isCorrect
+	return isCorrect
+}
+
+func CheckPasswordHashed(inputHashed []byte, bcryptString string) bool {
+	if len(inputHashed) != sha512.Size {
+		return false
+	}
+
+	if isCorrect, ok := passwordCache[string(inputHashed)]; ok {
+		return isCorrect
+	}
+
+	err := bcrypt.CompareHashAndPassword(
+		[]byte(bcryptString),
+		inputHashed,
+	)
+
+	isCorrect := err == nil
+	passwordCache[string(inputHashed)] = isCorrect
+	return isCorrect
+}
+
+func GetSHA512Hash(input string) []byte {
+	hash := sha512.New()
+	hash.Write([]byte(input))
+	hashedBytes := hash.Sum(nil)
+	return hashedBytes
+}
