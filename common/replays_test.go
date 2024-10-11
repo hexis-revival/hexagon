803c896d7280a41f1f4381ee37a3aa0e1d818215
Lekuru <contact@lekuru.xyz>
2024-10-11 18:50:01 +0200

    Extend replay testing to include serialization

--- common/replays_test.go
+++ common/replays_test.go
@@ -20,6 +20,7 @@ func TestReplayParsing(t *testing.T) {
 		return
 	}
 
+	// Deserialize replay from file
 	stream := NewIOStream(replayData, binary.BigEndian)
 	replay, err := ReadFullReplay(stream)
 	if err != nil {
@@ -38,4 +39,29 @@ func TestReplayParsing(t *testing.T) {
 	t.Cleanup(func() {
 		file.Close()
 	})
+
+	// Re-serialize replay
+	stream = NewIOStream([]byte{}, binary.BigEndian)
+	replay.Serialize(stream)
+
+	if len(stream.Get()) == 0 {
+		t.Error("failed to serialize replay")
+	}
+
+	// Deserialize replay from serialized data
+	stream.Seek(0)
+	replaySerialized, err := ReadFullReplay(stream)
+
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	if replaySerialized.Header == nil {
+		t.Error("failed to read replay header")
+	}
+
+	if len(replaySerialized.Frames) == 0 {
+		t.Error("failed to read replay frames")
+	}
 }
