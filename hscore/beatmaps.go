28715269c7098e9bf5562d7c36e3b080952ee879
Lekuru <contact@lekuru.xyz>
2024-10-17 22:16:25 +0200

    Implement `/web/gen-id.php` parsing

--- hscore/beatmaps.go
+++ hscore/beatmaps.go
@@ -1,3 +1,64 @@
 package hscore
 
-func BeatmapGenIdHandler(ctx *Context) {}
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"strings"
+
+	"github.com/hexis-revival/hexagon/common"
+)
+
+func BeatmapGenIdHandler(ctx *Context) {
+	request, err := NewBeatmapSubmissionRequest(ctx.Request)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	ctx.Server.Logger.Debugf("Beatmap submission request: %s", request)
+	// TODO: Implement logic
+}
+
+func NewBeatmapSubmissionRequest(request *http.Request) (*BeatmapSubmissionRequest, error) {
+	err := request.ParseMultipartForm(10 << 20) // ~10 MB
+	if err != nil {
+		return nil, err
+	}
+
+	username := GetMultipartFormValue(request, "u")
+	password := GetMultipartFormValue(request, "p")
+	setId := GetMultipartFormValue(request, "s")
+	beatmapIdsString := GetMultipartFormValue(request, "v")
+	clientVersion := GetMultipartFormValue(request, "x")
+
+	collection := common.NewErrorCollection()
+
+	setIdInt, err := strconv.Atoi(setId)
+	collection.Add(err)
+
+	clientVersionInt, err := strconv.Atoi(clientVersion)
+	collection.Add(err)
+
+	beatmapIdsList := strings.Split(beatmapIdsString, ":")
+	beatmapIds := make([]int, 0, len(beatmapIdsList))
+
+	for _, beatmapId := range beatmapIdsList {
+		beatmapIdInt, err := strconv.Atoi(beatmapId)
+		beatmapIds = append(beatmapIds, beatmapIdInt)
+		collection.Add(err)
+	}
+
+	if collection.HasErrors() {
+		return nil, collection.Pop(0)
+	}
+
+	return &BeatmapSubmissionRequest{
+		Username:      username,
+		Password:      password,
+		BeatmapIds:    beatmapIds,
+		SetId:         setIdInt,
+		ClientVersion: clientVersionInt,
+	}, nil
+}
