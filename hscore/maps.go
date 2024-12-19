e810bf11a5e07803c8a8387a2fad422dada3c594
Lekuru <contact@lekuru.xyz>
2024-12-19 19:07:10 +0100

    Implement update handler

--- hscore/maps.go
+++ hscore/maps.go
@@ -4,9 +4,42 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+
+	"github.com/hexis-revival/hexagon/common"
 )
 
-func BeatmapUpdateHandler(ctx *Context) {}
+func BeatmapUpdateHandler(ctx *Context) {
+	request, err := NewBeatmapUpdateRequest(*ctx.Request)
+	if err != nil {
+		ctx.Server.Logger.Errorf("Failed to parse beatmap update request: %s", err)
+		ctx.Response.WriteHeader(400)
+	}
+
+	user, err := common.FetchUserById(
+		request.UserId,
+		ctx.Server.State,
+	)
+
+	if err != nil {
+		ctx.Server.Logger.Errorf("Failed to fetch user: %s", err)
+		ctx.Response.WriteHeader(401)
+	}
+
+	ctx.Server.Logger.Debugf(
+		"Beatmap update request from '%s' (%d)",
+		user.Name, request.BeatmapId,
+	)
+
+	beatmapData, err := ctx.Server.State.Storage.GetBeatmapFile(request.BeatmapId)
+	if err != nil {
+		ctx.Server.Logger.Errorf("Failed to fetch beatmap data: %s", err)
+		ctx.Response.WriteHeader(404)
+	}
+
+	ctx.Response.WriteHeader(200)
+	ctx.Response.Header().Set("Content-Type", "application/xml")
+	ctx.Response.Write(beatmapData)
+}
 
 func NewBeatmapUpdateRequest(request http.Request) (*BeatmapUpdateRequest, error) {
 	queryParameters := request.URL.Query()
