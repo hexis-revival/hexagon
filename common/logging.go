a0e98cafdd92e11ac90d393ec7347c972ac12704
Lekuru <contact@lekuru.xyz>
2024-12-27 17:21:56 +0100

    Add anomaly logging

--- common/logging.go
+++ common/logging.go
@@ -11,7 +11,7 @@ import (
 )
 
 const (
-	QUIET   int = 50
+	ANOMALY int = 50
 	ERROR   int = 40
 	WARNING int = 30
 	INFO    int = 20
@@ -21,11 +21,26 @@ const (
 
 const (
 	ColorReset  = "\033[0m"
+	ColorBlack  = "\033[30m"
 	ColorRed    = "\033[31m"
+	ColorGreen  = "\033[32m"
 	ColorYellow = "\033[33m"
 	ColorBlue   = "\033[34m"
+	ColorPurple = "\033[35m"
+	ColorCyan   = "\033[36m"
 	ColorWhite  = "\033[37m"
-	ColorGrey   = "\033[90m"
+	ColorGrey   = "\033[0;90m"
+)
+
+const (
+	ColorBoldBlack  = "\033[1;30m"
+	ColorBoldRed    = "\033[1;31m"
+	ColorBoldGreen  = "\033[1;32m"
+	ColorBoldYellow = "\033[1;33m"
+	ColorBoldBlue   = "\033[1;34m"
+	ColorBoldPurple = "\033[1;35m"
+	ColorBoldCyan   = "\033[1;36m"
+	ColorBoldWhite  = "\033[1;37m"
 )
 
 type Logger struct {
@@ -63,7 +78,6 @@ func (c *Logger) Info(msg ...any) {
 	if c.level > INFO {
 		return
 	}
-
 	c.logger.Println(c.formatLogMessage("INFO", concatMessage(msg...)))
 }
 
@@ -71,7 +85,6 @@ func (c *Logger) Infof(format string, msg ...any) {
 	if c.level > INFO {
 		return
 	}
-
 	c.logger.Println(c.formatLogMessage("INFO", fmt.Sprintf(format, msg...)))
 }
 
@@ -86,7 +99,6 @@ func (c *Logger) Errorf(format string, msg ...any) {
 	if c.level > ERROR {
 		return
 	}
-
 	c.logger.Println(c.formatLogMessage("ERROR", fmt.Sprintf(format, msg...)))
 }
 
@@ -101,7 +113,6 @@ func (c *Logger) Warningf(format string, msg ...any) {
 	if c.level > WARNING {
 		return
 	}
-
 	c.logger.Println(c.formatLogMessage("WARNING", fmt.Sprintf(format, msg...)))
 }
 
@@ -116,7 +127,6 @@ func (c *Logger) Debugf(format string, msg ...any) {
 	if c.level > DEBUG {
 		return
 	}
-
 	c.logger.Println(c.formatLogMessage("DEBUG", fmt.Sprintf(format, msg...)))
 }
 
@@ -131,8 +141,14 @@ func (c *Logger) Verbosef(format string, msg ...any) {
 	if c.level > VERBOSE {
 		return
 	}
-
 	c.logger.Println(c.formatLogMessage("VERBOSE", fmt.Sprintf(format, msg...)))
+}
+
+func (c *Logger) LogAnomaly(msg ...any) {
+	if c.level > ANOMALY {
+		return
+	}
+	c.logger.Println(c.formatLogMessage("ANOMALY", concatMessage(msg...)))
 }
 
 func (c *Logger) formatLogMessage(level string, msg string) string {
@@ -155,6 +171,8 @@ func getColor(level string) string {
 		return ColorBlue
 	case "ERROR":
 		return ColorRed
+	case "ANOMALY":
+		return ColorBoldYellow
 	case "WARNING":
 		return ColorYellow
 	case "DEBUG":
